f72c1a1a2e904626ce7d652f1e37f63f61749e9c
aerodinamicat <aerodinamicat@gmail.com>
2022-12-01 08:25:11 +0100

    'Let' changed to 'const' where needed

--- src/handlers/banks.handler.ts
+++ src/handlers/banks.handler.ts
@@ -5,25 +5,27 @@ const urlApi = 'http://localhost:6789'
 export async function handler(req: Request, res: Response) {
   const id = parseInt(req.params['id'])
 
-  let [fetchedBanks, fetchedCountries, fetchedCurrencies] = await Promise.all([
-    fetchData(`${urlApi}/banks`),
-    fetchData(`${urlApi}/countries`),
-    fetchData(`${urlApi}/currencies`),
-  ])
+  const [fetchedBanks, fetchedCountries, fetchedCurrencies] = await Promise.all(
+    [
+      fetchData(`${urlApi}/banks`),
+      fetchData(`${urlApi}/countries`),
+      fetchData(`${urlApi}/currencies`),
+    ],
+  )
 
-  let foundBank = fetchedBanks.find((item: any) => item.id === id)
+  const foundBank = fetchedBanks.find((item: any) => item.id === id)
   if (!foundBank) {
     return res.status(404).send({
       message: `Bank #${id} not found`,
     })
   }
 
-  let currencyNames = foundBank.currencies.map(
-    (currencyId: any) =>
+  const currencyNames = foundBank.currencies.map(
+    (currencyId: number) =>
       fetchedCurrencies.find((item: any) => item.id === currencyId).name,
   )
-  let countryNames = foundBank.countries.map(
-    (countryId: any) =>
+  const countryNames = foundBank.countries.map(
+    (countryId: number) =>
       fetchedCountries.find((item: any) => item.id === countryId).name,
   )
 
